739d0a7a38a31c7d6edb898e41760684c680212f
Lei Chen <mashirocl@gmail.com>
2022-10-26 21:47:47 +0900

    Update: user requests to take place 3rd package pygit2 use rate limit in request header to decide pause api using update corresponding tests

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,8 @@
 import unittest
 import os
 from github import Github
-from core.main import search, MailMap
+from core.githuber import GitHuber
+from core.main import search, MailMap, add_name_email, search_top_user
 from core.repository import Repository
 
 github = Github(user_agent="mashirocl", password=os.getenv("MAILMAPBUILDER"))
@@ -13,21 +14,31 @@ class MyTestCase(unittest.TestCase):
         githuber = search("Benjamin Diedrichsen", github)
         self.assertEqual(githuber.login, "bennidi")
 
-    def test_search_2(self):
-        githuber = search("Andreas Perhab", github)
-        print(githuber.login)
-        print(githuber.names)
-        print(githuber.emails)
+    def test_search2(self):
+        githuber = search("Andreas Perhab")
+        self.assertEqual(githuber.login,"bigbear3001")
 
-    def test_build_githubers(self):
-        githuber = search("Andreas Perhab", github)
+    def test_add_name_email(self):
+        githuber = GitHuber("mashirocl")
+        add_name_email(githuber)
+
+    # def test_search_rate_limit(self):
+    #     for i in range(60):
+    #         search_top_user("Benjamin Diedrichsen")
+
+    def test_search_top_user(self):
+        url = search_top_user("Benjamin Diedrichsen")["url"]
+        self.assertEqual("https://api.github.com/users/bennidi", url)
+
+    def test_search(self):
+        githuber = search("bennidi")
+        self.assertTrue("b.diedrichsen@gmail.com" in githuber.emails)
 
     def test_mailmap_building(self):
         path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador"
         repo = Repository(path)
         mailmap = MailMap(repo)
         mailmap.build_mailmap(".mailmap")
-
 
 if __name__ == '__main__':
     unittest.main()
