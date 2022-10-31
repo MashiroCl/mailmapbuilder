befe09455ad8ca4ab4aa78385b193a8a0a60844e
Lei Chen <mashirocl@gmail.com>
2022-10-31 15:25:07 +0900

    Update: test for split shortlog

--- test/test_repository.py
+++ test/test_repository.py
@@ -9,13 +9,20 @@ class MyTestCase(unittest.TestCase):
         r = Repository(path)
 
     def test_get_useremails(self):
-        path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador"
+        path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/jsoup"
         r = Repository(path)
         d = r.get_useremails()
+        print("================")
         print(d)
+        print("================")
         self.assertEqual({'benjamin.diedrichsen@okotta.com', 'b.diedrichsen@googlemail.com', 'b.diedrichsen@gmail.com'},
                          d.get("Benjamin Diedrichsen"))
 
+    def test_split_shortlog(self):
+        path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador"
+        r = Repository(path)
+        res = r._split_shortlog(["\t48	Benjamin Diedrichsen <b.diedrichsen@gmail.com>"])
+        print(res)
 
 if __name__ == '__main__':
     unittest.main()
